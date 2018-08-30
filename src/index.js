1cc50ddfd87ac20d65aa5e9bdd2aa6c8b3cb2ca0
WhyK <mcgradyhaha@gmail.com>
2018-08-30 22:07:30 +1000

    web app updated

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+    let element = document.createElement('div');
+    var btn = document.createElement('button');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
+  }
+  
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      titile: 'Output Management'
+    })
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+
+  
+};
