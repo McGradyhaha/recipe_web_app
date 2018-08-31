9b6649be09f8335694ab4bdcd53495c955ea9e7b
WhyK <mcgradyhaha@gmail.com>
2018-08-31 20:27:00 +1000

    fix react bug

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.jsx'
     
   },
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,41 +1,40 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const webpack = require('webpack')
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const webpack = require("webpack");
 
-module.exports = merge(common,{
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
 
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',  
-            options: {
-                cacheDirectory: true,
-                plugins: ['react-hot-loader/babel']
-            }
-          },
-          {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
-          },
-          {
-            test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, 
-            loader:'url-loader',
-            options:{
-                limit:8000
-            } 
-          }
-        ]
-    },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+  module: {
+    rules: [
+      {
+        // babel is for compiling ES6, jsx to ES5
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: true,
+          plugins: ["react-hot-loader/babel"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i,
+        loader: "url-loader",
+        options: {
+          limit: 8000
+        }
+      }
     ]
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 });
